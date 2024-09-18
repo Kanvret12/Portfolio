1b54d07582baaf4fae5c75a9e329d40607fcc3a4
Kanvret12 <82053023+Kanvret12@users.noreply.github.com>
2024-09-18 09:23:37 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -155,7 +155,7 @@ export default function Home() {
           </div>
         </section>
       </main>
-      <nav className="fixed bottom-4 left-1/2 transform -translate-x-1/2 text-white py-3 px-6 bg-background rounded-full [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]">
+      <nav className="fixed bottom-4 left-1/2 transform -translate-x-1/2 text-white py-3 px-6 bg-background bg-black rounded-full [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]">
         <div className="container mx-auto  px-4 flex justify-center">
           <ul className="flex space-x-7">
             <li>
